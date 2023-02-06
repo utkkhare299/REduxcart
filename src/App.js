716eaf65989dc9e398c319136cf983a2b35f4e59
utkarsh khare <utkkhare999@gmail.com>
2023-02-06 14:51:51 +0530

    Add to Cart Fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch('https://fir-99cf8-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-      method: 'PUT',
+    fetch("https://ecommerce-d98e4-default-rtdb.firebaseio.com/", {
+      method: "PUT",
       body: JSON.stringify(cart),
     });
   }, [cart]);
